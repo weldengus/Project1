2c009858c39080184d1d4bdc6471a3a627698ac8
Ezra ITC <Ezra ITC@Ezra.lan>
2024-01-05 19:59:26 -0500

    added comment commit and push

--- src/Demo/TestDemo.java
+++ src/Demo/TestDemo.java
@@ -1,11 +1,17 @@
 package Demo;
 
 public class TestDemo {
+	// Ezra weldengus
 public static void main(String[] args) {
 	// this what we were looking for.
 	int a=41;
 	System.out.println(a);
 	System.out.println("feeling hopful");
 	
+}
+// add new code
+
+public void add(int a,int b) {
+	System.out.println(a+b);
 }
 }
