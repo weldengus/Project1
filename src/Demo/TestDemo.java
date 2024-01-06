08ae548d0a170c45f44279598f9598909dc82468
weldengus <155380210+weldengus@users.noreply.github.com>
2024-01-05 19:06:26 -0500

    Update TestDemo.java

--- src/Demo/TestDemo.java
+++ src/Demo/TestDemo.java
@@ -2,7 +2,7 @@ package Demo;
 
 public class TestDemo {
 public static void main(String[] args) {
-	// this what we were looking for ezra.
+	// this what we were looking for.
 	int a=41;
 	System.out.println(a);
 	System.out.println("feeling hopful");
